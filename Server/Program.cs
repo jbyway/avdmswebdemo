ffb20b2b6a6de6a97dba5dba65669b6639cd799e
jbyway <30424593+jbyway@users.noreply.github.com>
2023-09-12 07:59:36 +1000

    asdadsf

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient();
+
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
